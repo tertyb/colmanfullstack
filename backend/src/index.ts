a432d6321085cae89a8e287a2032f951c246af26
Daniel Tzvi <danieltzvi10@gmail.com>
2024-11-23 12:24:59 +0200

    start auth router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,28 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors'
+import { config } from './config/config';
+import dotenv from 'dotenv'
+import { authRouter } from './controllers/authController';
+import connectDB from './config/db';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = config.PORT;
 
 app.use(express.json());
+
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+connectDB()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
+
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/src/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+  name: string;
+  image: string
+}
+
+const UserSchema = new Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  image: {type: String, required: false}
+});
+
+const UserModel = mongoose.model<IUser>('User', UserSchema);
+
+export default UserModel;
